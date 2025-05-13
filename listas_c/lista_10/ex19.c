247475b86f6d7ab72f3ce1754c153c6132e91b78
gkbrum <gabriel.krugerbrum@gmail.com>
2025-05-13 00:38:35 -0300

    consertando ex19.c

--- listas_c/lista_10/ex19.c
+++ listas_c/lista_10/ex19.c
@@ -4,59 +4,55 @@
 #define TAM 30
 
 int BuscaNome( char **lista, int tam );
-void TrocaNome( char **lista, int pos );
-void CriaLista( char **lista );
-void AddNome( char ***lista, int num );
-void RemNome( char **lista, int tam, int pos );
+char **TrocaNome( char **lista, int pos );
+char **CriaLista();
+char **AddNome( char **lista, int tam );
+char **RemNome( char **lista, int tam, int pos );
 int Menu();
 void LerString( char *string );
 void LimparBuffer();
 
 int main(){
-    int op, tamLista = 1, numElementos = 0, pos;
+    int op, tamLista = 0, pos;
     char **lista = NULL;
 
     do{
         op = Menu();
-        pos = -1;
 
         switch ( op ){
         case 1:
             printf( "\nDigite o nome a ser adicionado:" );
-            AddNome( &lista, numElementos );
-            numElementos++;
+            lista = AddNome( lista, tamLista );
             tamLista++;
             break;
         
         case 2:
-            if( numElementos == 0 ){
+            if( tamLista == 0 ){
                 printf( "\nLista vazia." );
             } else {
                 printf( "\nDigite a posição do nome a ser excluido (1 ~ %d): ", tamLista );
                 scanf( "%d", &pos );
                 LimparBuffer();
 
-                
-                RemNome( lista, tamLista, pos );
-                numElementos--;
+                lista = RemNome( lista, tamLista, pos );
                 tamLista--;
             }
             break;
 
         case 3:
-            if( numElementos == 0 ){
+            if( tamLista == 0 ){
                 printf( "\nLista vazia." );
             } else {
                 printf( "\nDigite a posição a ser alterada (1 ~ %d): ", tamLista );
                 scanf( "%d", &pos );
                 LimparBuffer();
 
-                TrocaNome( lista, pos );
+                lista = TrocaNome( lista, pos );
             }
             break;
 
         case 4:
-            if( numElementos == 0 ){
+            if( tamLista == 0 ){
                 printf( "\nLista vazia." );
             } else {
                 printf( "\nDigite o nome a ser removido: " );
@@ -64,32 +60,33 @@ int main(){
                 if( pos == -1 ){
                     printf( "\nEsse nome não esta na lista. " );
                 } else {
-                    RemNome( lista, tamLista, pos );
-                    numElementos--;
+                    lista = RemNome( lista, tamLista, pos );
                     tamLista--;
                 }
             }
             break;
 
         case 5:
-            if( numElementos == 0 ){
+            if( tamLista == 0 ){
                 printf( "\nLista vazia." );
             } else {
                 printf( "\nDigite a posição a ser mostrada na tela (1 ~ %d): ", tamLista );
                 scanf( "%d", &pos );
                 LimparBuffer();
 
-                printf( "\n%d. %s.", pos + 1, lista[ pos ] );
+                printf( "\n%d. %s.", pos, lista[ pos - 1 ] );
             }
             break;
 
         case 6:
-            if( numElementos == 0 ){
+            if( tamLista == 0 ){
                 printf( "\nLista vazia." );
             } else {
+                printf( "\n" );
                 for( int i = 0; i < tamLista; i++ ){
-                    printf( "\n%d. %s", i + 1, lista[ i ] );
+                    printf( "%d. %s", i + 1, lista[ i ] );
                 }
+                printf( "\n" );
             } 
             break;
             
@@ -109,40 +106,54 @@ int BuscaNome( char **lista, int tam ){
     for (int i = 0; i <= tam; i++){
         if ( i == tam ){
             return -1;
-        } else if ( strcmp( lista[i], nome ) ){ 
-            return i;
+        } else if ( strcmp( lista[i], nome ) == 0 ){ 
+            return ( i + 1 );
         }
     }
     return -1;
 }
 
-void TrocaNome( char **lista, int pos ){
+char **TrocaNome( char **lista, int pos ){
     printf( "\nDigite um novo nome para a posicao %d:", pos );
-    LerString( lista[ pos ] );
+    LerString( lista[ pos - 1 ] );
+    return lista;
 }
 
-void CriaLista( char **lista ){
+char **CriaLista(){
+    char **lista;
+
     lista = ( char ** )malloc( sizeof( char * ) );
     lista[0] = ( char * )malloc( sizeof( char ) * TAM );
+    
+    return lista;
 }
 
-void AddNome( char ***lista, int num ){
+char **AddNome( char **lista, int tam ){
     if( lista ){
-        *lista = ( char *** )realloc( lista, (sizeof( lista )) + sizeof( char * ) );
-        *lista[ num + 1] = ( char ** )malloc( sizeof( char ) * TAM );
-        LerString( *lista[ num ] );
+        lista = ( char ** )realloc( lista, ( tam + 1 ) * sizeof( char * ) );
+        lista[ tam ] = ( char * )malloc( sizeof( char ) * TAM );
+        LerString( lista[ tam ] );
+        
+        return lista;
+
     } else {
-        CriaLista( *lista );
-        LerString( *lista[ num ] );
+        lista = CriaLista();
+        LerString( lista[ tam ] );
+
+        return lista;
     }
 }
 
-void RemNome( char **lista, int tam, int pos ){ 
-    for( int i = pos; i != tam; i++ ){
+char **RemNome( char **lista, int tam, int pos ){ 
+
+    for( int i = ( pos - 1 ); i < ( tam - 1 ); i++ ){
         strcpy( lista[ i ], lista[ i + 1 ] );
     }
+    free( lista[ tam - 1 ] );
 
-    lista = ( char ** )realloc( lista, (sizeof( lista )) - sizeof( char * ) );
+    lista = ( char ** )realloc( lista, ( tam - 1 ) * sizeof( char * ) );
+
+    return lista;
 }
 
 int Menu(){
